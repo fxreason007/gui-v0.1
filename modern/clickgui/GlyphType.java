1093995cf2aff959ac7e5da3f8db6900f80af4bf
cxwpxz <cwspud@yandex.ru>
2025-07-20 04:44:57 +0300

    Create GlyphType.java

---
+++ modern/clickgui/GlyphType.java
@@ -0,0 +1,12 @@
+package exalted.zxc.ui.modern.clickgui;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import exalted.zxc.module.api.Module;
+
+
+public interface GlyphType {
+    Map<Character, List<Module>> get();
+} 
